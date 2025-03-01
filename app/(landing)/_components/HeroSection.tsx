9c9d1ce35dded7770973c209ae50572603b88ce3
HoangDucBach <hoangbach0985@gmail.com>
2025-03-01 16:04:27 +0700

    add(landpage): hero section

--- app/(landing)/_components/HeroSection.tsx
+++ app/(landing)/_components/HeroSection.tsx
@@ -1,14 +1,31 @@
+"use client";
+
+import { CAFButton } from "@/components/ui/button";
+import { Link } from "@heroui/link";
+import { IoPlayCircle } from "react-icons/io5";
+
 interface Props extends React.HTMLAttributes<HTMLDivElement> { }
 export const HeroSection: React.FC<Props> = () => {
     return (
-        <section id="hero" className="h-screen w-full bg-primary flex flex-col justify-center items-center">
-            <div className="flex flex-row gap-4">
-                <h1>
-                    <span>Ca</span>
+        <section id="hero" className="w-full h-full flex flex-row justify-center items-center">
+            <div className="flex flex-col gap-4 flex-1 text-default">
+                <h1 className="text-9xl font-bold">
+                    <span className="text-primary">Ca</span>
                     <span>Fi</span>
                 </h1>
-                <p>Social PvP Game</p>
-                <p>CoFi Game is a cafe business simulation game where players will play one of the types of companies in the cafe ecosystem and interact with other players to build a competitive market.</p>
+                <p className="text-secondary text-4xl font-bold">Social PvP Game</p>
+                <p className="w-full max-w-screen-sm font-medium break-words">CoFi Game is a cafe business simulation game where players will play one of the types of companies in the cafe ecosystem and interact with other players to build a competitive market.</p>
+                <div className="flex flex-row gap-4 items-center w-fit">
+                    <CAFButton
+                        className="w-fit"
+                        startContent={<IoPlayCircle size={24} />}
+                    >
+                        Play
+                    </CAFButton>
+                    <Link href="/about" showAnchorIcon color="foreground">Learn More</Link>
+                </div>
+            </div>
+            <div className="flex flex-col gap-4">
             </div>
         </section>
     )
