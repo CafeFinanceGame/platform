c1302f1f22920e0449619ce552e23afcf04f23ba
HoangDucBach <hoangbach0985@gmail.com>
2025-03-04 20:09:50 +0700

    add(types): test

--- types/index.ts
+++ types/index.ts
@@ -3,3 +3,15 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+enum PlayerRole {
+  COFFEE_COMPANY,
+  MACHINE_COMPANY,
+  MATERIAL_COMPANY
+}
+
+type Company = {
+  owner: string;
+  role: PlayerRole;
+  energy: number;
+}
