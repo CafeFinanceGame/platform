aa6c1ee7f4c475869cb20d5c4cb4d72c7a7882ff
HoangDucBach <hoangbach0985@gmail.com>
2025-03-05 15:58:33 +0700

    modify(hooks): game economy

--- hooks/useCAFDependency.tsx
+++ hooks/useCAFDependency.tsx
@@ -1,4 +1,4 @@
-import {readContract} from "@wagmi/core";
+import { readContract } from "@wagmi/core";
 import { ProductItemType, ProductEconomy, ActivityEnergyFee, ListedItem } from "@/types";
 import constants from '@/utils/constants';
 import wagmi from "@/utils/wagmi";
@@ -20,91 +20,19 @@ interface ICAFGameEconomyActions {
 }
 
 interface ICAFGameEconomyParameters {
-    coffeeCompany: {
-        coffeeBean: {
-            energy: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-            manufacturedPerHour: number;
-        };
-        blackCoffee: {
-            energy: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-        };
-        milkCoffee: {
-            energy: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-        };
-    };
-    materialCompany: {
-        poweredMilk: {
-            energy: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-            manufacturedPerHour: number;
-        };
-        milk: {
-            energy: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-        };
-        water: {
-            energy: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-            manufacturedPerHour: number;
-        };
-    };
-    machineCompany: {
-        materialMachine: {
-            durability: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-            manufacturedPerHour: number;
-        };
-        kettle: {
-            durability: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-        };
-        milkFrother: {
-            durability: number;
-            decayRate: number;
-            decayPeriod: string;
-            costPrice: number;
-            insurancePrice: number;
-            freightPrice: number;
-        };
-    };
-}
+    COFFEE_BEAN: ProductEconomy;
+    BLACK_COFFEE: ProductEconomy;
+    MILK_COFFEE: ProductEconomy;
+    POWERED_MILK: ProductEconomy;
+    MILK: ProductEconomy;
+    WATER: ProductEconomy;
+    MATERIAL_MACHINE: ProductEconomy;
+    KETTLE: ProductEconomy;
+    MILK_FOTHER: ProductEconomy;
+};
 
-export const useCAFGameEconomy = (): ICAFGameEconomyActions => {
+interface ICAFGameEconomyHook extends ICAFGameEconomyActions, ICAFGameEconomyParameters { }
+export const useCAFGameEconomy = (): ICAFGameEconomyHook => {
     const account = useAccount();
 
     return {
@@ -157,6 +85,92 @@ export const useCAFGameEconomy = (): ICAFGameEconomyActions => {
                 console.error('Error getting product economy', error);
                 throw error;
             }
+        },
+
+        COFFEE_BEAN: {
+            energy: 50,
+            durability: 0,
+            decayRate: 1,
+            decayPeriod: 3,
+            costPrice: 5,
+            insurancePrice: 1,
+            freightPrice: 1,
+            manufacturedPerHour: 10
+        },
+        BLACK_COFFEE: {
+            energy: 100,
+            durability: 0,
+            decayRate: 1,
+            decayPeriod: 2,
+            costPrice: 10,
+            insurancePrice: 1,
+            freightPrice: 1
+        },
+        MILK_COFFEE: {
+            energy: 250,
+            durability: 0,
+            decayRate: 6,
+            decayPeriod: 1,
+            costPrice: 20,
+            insurancePrice: 3,
+            freightPrice: 4
+        },
+        POWERED_MILK: {
+            energy: 180,
+            durability: 0,
+            decayRate: 4,
+            decayPeriod: 1,
+            costPrice: 9,
+            insurancePrice: 1,
+            freightPrice: 2,
+            manufacturedPerHour: 20
+        },
+        MILK: {
+            energy: 120,
+            durability: 0,
+            decayRate: 3,
+            decayPeriod: 1,
+            costPrice: 8,
+            insurancePrice: 1,
+            freightPrice: 2
+        },
+        WATER: {
+            energy: 30,
+            durability: 0,
+            decayRate: 1,
+            decayPeriod: 6,
+            costPrice: 3,
+            insurancePrice: 1,
+            freightPrice: 1,
+            manufacturedPerHour: 30
+        },
+        MATERIAL_MACHINE: {
+            energy: 0,
+            durability: 500,
+            decayRate: 3,
+            decayPeriod: 1,
+            costPrice: 50,
+            insurancePrice: 5,
+            freightPrice: 8,
+            manufacturedPerHour: 5
+        },
+        KETTLE: {
+            energy: 0,
+            durability: 400,
+            decayRate: 2,
+            decayPeriod: 1,
+            costPrice: 40,
+            insurancePrice: 4,
+            freightPrice: 6
+        },
+        MILK_FOTHER: {
+            energy: 0,
+            durability: 600,
+            decayRate: 4,
+            decayPeriod: 1,
+            costPrice: 60,
+            insurancePrice: 6,
+            freightPrice: 10,
         }
     }
 }
